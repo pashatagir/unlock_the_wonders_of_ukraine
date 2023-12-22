0c4ccd23be55d9b71fadbcace54408b25de3dda0
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-12-22 22:14:56 +0200

    adds smooth scroll to anchors

--- src/js/backToHome.js
+++ src/js/backToHome.js
@@ -1,41 +1,22 @@
 // Set a variable for our button element.
-const scrollToTopButton = document.querySelector('.back-to-home');
-smoothScrollCapture = (x, e) => {
-  e.preventDefault();
-  var id = target.getAttribute('href').split('#')[1];
-  var el = document.getElementById(id);
-  var x = el.getBoundingClientRect().left;
-  var y = el.getBoundingClientRect().top;
-  smoothScrollFunc(x, y);
-};
+const backHomeEl = document.querySelector('.back-to-home');
+const anchorEl = document.querySelectorAll('a[href^="#"]');
 
-smoothScrollFunc = (x, y, e) => {
-  e.preventDefault();
-  window.scrollTo({
-    top: y,
-    left: x,
-    behavior: 'smooth',
+anchorEl.forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
   });
-};
-
-window.addEventListener('DOMContentLoaded', () => {
-  scrollToTopButton.addEventListener('click', e => {
-    smoothScrollFunc(0, 0, e);
-  });
-
-  let aTags = document.getElementsByTagName('A');
-  for (let i = 0; i < aTags.length; i += 1) {
-    if (aTags[i].getAttribute('href').indexOf('#') === 0) {
-      aTags[i].addEventListener('click', smoothScrollCapture);
-    }
-  }
 });
 
-window.addEventListener('scroll', e => {
+window.addEventListener('scroll', () => {
   let y = window.scrollY;
-  if (y > 800) {
-    scrollToTopButton.style.display = 'block';
+  if (y > 850) {
+    backHomeEl.style.display = 'block';
   } else {
-    scrollToTopButton.style.display = 'none';
+    backHomeEl.style.display = 'none';
   }
 });
