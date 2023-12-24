b5c824aa680d53f6cf4a3379dd2b9a0ae14293b1
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-12-25 01:20:50 +0200

    fixes slider and adds tablet style

--- src/js/swiperSlider.js
+++ src/js/swiperSlider.js
@@ -3,9 +3,45 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/scss';
 
 const swiper = new Swiper('.swiper', {
-  // Install modules
   modules: [Navigation],
   speed: 500,
+  grabCursor: true,
+  slidesPerView: 1,
+  spaceBetween: 16,
+  breakpoints: {
+    835: {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiperGallery = new Swiper('.swiper-gallery', {
+  modules: [Navigation],
+  speed: 500,
+  grabCursor: true,
+  slidesPerView: 1,
+  spaceBetween: 16,
+  breakpoints: {
+    835: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiperTour = new Swiper('.swiper-tour', {
+  modules: [Navigation],
+  speed: 500,
+  grabCursor: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
