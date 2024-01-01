ed541ccdcbe00dedb190af47fa9f473e68819d37
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2024-01-01 17:53:03 +0200

    small fix

--- src/js/backToHome.js
+++ src/js/backToHome.js
@@ -5,9 +5,7 @@ anchorEl.forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
 
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth',
-    });
+    document.querySelector(this.getAttribute('href')).scrollIntoView();
   });
 });
 
