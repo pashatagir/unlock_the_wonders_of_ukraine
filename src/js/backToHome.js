95e411763a46d801a130d248d683d2c4c0a794f0
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2024-01-01 17:19:24 +0200

    some fix

--- src/js/backToHome.js
+++ src/js/backToHome.js
@@ -1,21 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const anchorEl = document.querySelectorAll('a[href^="#"]');
+const backHomeEl = document.querySelector('.to-top');
+const anchorEl = document.querySelectorAll('a[href^="#"]');
 
-  anchorEl.forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+anchorEl.forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
 
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth',
-      });
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
     });
   });
 });
 
 window.addEventListener('scroll', () => {
-  const backHomeEl = document.querySelector('.to-top');
   let y = window.scrollY;
-
   if (y > 1000) {
     backHomeEl.style.display = 'block';
   } else {
