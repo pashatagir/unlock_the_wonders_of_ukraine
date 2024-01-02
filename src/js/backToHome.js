df80bb9968f286e57666065b03d47bf96c36865c
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2024-01-02 20:00:42 +0200

    one more attempt to disable scrolling in pre-15.4 Safari when the mobile menu is open

--- src/js/backToHome.js
+++ src/js/backToHome.js
@@ -1,11 +1,13 @@
+import smoothscroll from 'smoothscroll-polyfill';
+
 const backHomeEl = document.querySelector('.to-top');
 const anchorEl = document.querySelectorAll('a[href^="#"]');
 
 anchorEl.forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
-
     document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    smoothscroll.polyfill();
   });
 });
 
