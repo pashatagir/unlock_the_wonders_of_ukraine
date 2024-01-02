77cf184a51f1056565d268f37c72c92455aac8e2
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2024-01-02 11:42:07 +0200

    adds smoothscroll-polyfill for Safari before version 15.4

--- src/js/backToHome.js
+++ src/js/backToHome.js
@@ -1,10 +1,12 @@
+import smoothscroll from 'smoothscroll-polyfill';
+
 const backHomeEl = document.querySelector('.to-top');
 const anchorEl = document.querySelectorAll('a[href^="#"]');
 
 anchorEl.forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
-
+    smoothscroll.polyfill();
     document.querySelector(this.getAttribute('href')).scrollIntoView();
   });
 });
