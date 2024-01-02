b7056b2da2e2040c7ddd3527a1916d16ae6b8f70
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2024-01-02 20:27:00 +0200

    it doesnt work

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -2,25 +2,6 @@ const menuButtonEl = document.querySelectorAll('.header__button');
 const menuEl = document.querySelector('.mobile-menu');
 const menuLinksEl = document.querySelectorAll('.mobile-menu__link');
 
-// disabled scroll in Safari version before 15.4 when mobile-menu open
-if (document.body.classList.contains('no-scroll')) {
-  document.addEventListener(
-    'touchmove',
-    function (e) {
-      e.preventDefault();
-    },
-    { passive: false }
-  );
-
-  document.addEventListener(
-    'wheel',
-    function (e) {
-      e.preventDefault();
-    },
-    { passive: false }
-  );
-}
-
 menuButtonEl.forEach(item =>
   item.addEventListener('click', e => {
     menuEl.classList.toggle('is-open');
