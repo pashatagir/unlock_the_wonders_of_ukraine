e4d77a83207e61ca495644914a0ebc3f7a60b4fe
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2024-01-04 13:25:18 +0200

    adds smoothscroll polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,32 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV == 'production';
 
-const stylesHandler = isProduction
-  ? MiniCssExtractPlugin.loader
-  : "style-loader";
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
 
 const config = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "js/[name].[contenthash].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[contenthash].bundle.js',
     clean: true,
   },
   devServer: {
     open: true,
-    host: "localhost",
-    port: 3000,
+    host: 'localhost',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./src") + "/index.html",
+      template: path.resolve(__dirname, './src') + '/index.html',
     }),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "./src") + "/images",
-          to: path.resolve(__dirname, "./dist") + "/images",
+          from: path.resolve(__dirname, './src') + '/images',
+          to: path.resolve(__dirname, './dist') + '/images',
         },
       ],
     }),
@@ -38,19 +35,19 @@ const config = {
     rules: [
       {
         test: /\.(js|jsx)$/i,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "sass-loader"],
+        use: [stylesHandler, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: "asset/inline",
+        type: 'asset/inline',
       },
     ],
   },
@@ -63,16 +60,16 @@ const config = {
 
 module.exports = () => {
   if (isProduction) {
-    config.mode = "production";
+    config.mode = 'production';
 
     config.plugins.push(
       new MiniCssExtractPlugin({
-        filename: "styles/[name].[contenthash].css",
-        chunkFilename: "[id].css",
+        filename: 'styles/[name].[contenthash].css',
+        chunkFilename: '[id].css',
       })
     );
   } else {
-    config.mode = "development";
+    config.mode = 'development';
   }
   return config;
 };
